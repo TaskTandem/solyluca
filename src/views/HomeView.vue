c5bf570873eb2bd308f3d984afef42d3b6818501
Francisco Macedonio <macedoniofrancisco@hotmail.com>
2024-06-26 18:57:29 -0300

    whatsapp

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,7 +8,7 @@ import { RouterView } from 'vue-router';
     <div class="about">
       <p>Distribuidora de artículos de limpieza, tabaquería y accesorios.</p>
       <p>PROVINCIA DE BUENOS AIRES, ARGENTINA</p>
-      <p>Contactanos: +54 9 223 339-5099</p>
+      <p>Contactanos: <a href="https://wa.me/2233395099" target="_blank">223 339-5099<fa :icon="['fab', 'whatsapp']" /></a></p>
     </div>
     <div class="section">
       <RouterView :key="$route.fullPath" />
